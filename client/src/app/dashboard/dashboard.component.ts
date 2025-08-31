8857e953affd37e1f9828123448a91d8183538e2
Ali Saleh <alisaleh06@outlook.com.au>
2025-08-31 20:26:03 +1000

    added uuid support

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -6,61 +6,87 @@ import { Router } from '@angular/router';
 import { User } from '../models/user.model';
 import { AuthService } from '../services/auth.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css'],
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  groups: Group[] = [];
-  user?: User;
-  currentUserKey = 'user';
-  @ViewChild('groupUsersModal') groupUsersModal!: TemplateRef<any>;
-  selectedGroup?: Group;
-  selectedGroupUsers?: User[];
+    // Initial variables...
+    groups: Group[] = [];
+    user?: User;
+    currentUserKey = 'user';
+    selectedGroup?: Group;
+    selectedGroupUsers?: User[];
+    newGroupInfo?: Group = {
+        id: uuidv4(),
+    };
 
-  constructor(
-    private groupService: GroupService,
-    private router: Router,
-    private storage: StorageService,
-    private auth: AuthService,
-    private modalService: NgbModal
-  ) {}
+    // modals
+    @ViewChild('groupUsersModal') groupUsersModal!: TemplateRef<any>;
+    @ViewChild('newGroupModal') newGroupModal!: TemplateRef<any>;
 
-  ngOnInit(): void {
-    const currentUser = this.storage.load(this.currentUserKey) as User | null;
-    console.log(currentUser);
-    if (!currentUser) {
-      this.router.navigate(['/']);
-      return;
+    constructor(
+        private groupService: GroupService,
+        private router: Router,
+        private storage: StorageService,
+        private auth: AuthService,
+        private modalService: NgbModal
+    ) {}
+
+    ngOnInit(): void {
+        const currentUser = this.storage.load(
+            this.currentUserKey
+        ) as User | null;
+        console.log(currentUser);
+        if (!currentUser) {
+            this.router.navigate(['/']);
+            return;
+        }
+        this.user = currentUser;
+        this.groups = this.groupService.getGroups();
     }
-    this.user = currentUser;
-    this.groups = this.groupService.getGroups();
-  }
 
-  logout(): void {
-    this.auth.logout();
-    this.router.navigate(['/']);
-  }
+    logout(): void {
+        this.auth.logout();
+        this.router.navigate(['/']);
+    }
 
-  openGroupModal(group: Group) {
-    this.selectedGroup = group;
-    this.getGroupUserInfo(group);
-    this.modalService.open(this.groupUsersModal, { centered: true });
-  }
+    openGroupModal(group: Group) {
+        this.selectedGroup = group;
+        this.getGroupUserInfo(group);
+        this.modalService.open(this.groupUsersModal, { centered: true });
+    }
 
-  getGroupUserInfo(group: Group) {
-    // get all users
-    const allUsers = this.auth.getAllUsers();
+    getGroupUserInfo(group: Group) {
+        // get all users
+        const allUsers = this.auth.getAllUsers();
 
-    // check which users are in this group through user ID
-    const groupUsers = allUsers?.filter((u) =>
-      group.memberUserIds.includes(u.id)
-    );
-    console.log('Group ' + group.name + ' has these users: ');
-    console.log(groupUsers);
+        // check which users are in this group through user ID
+        const groupUsers = allUsers?.filter((u) =>
+            group.memberUserIds.includes(u.id)
+        );
+        console.log('Group ' + group.name + ' has these users: ');
+        console.log(groupUsers);
 
-    this.selectedGroupUsers = groupUsers;
-  }
+        this.selectedGroupUsers = groupUsers;
+    }
+
+    openNewGroupModal() {
+        this.modalService.open(this.newGroupModal, { centered: true });
+    }
+
+    newGroup() {
+        const groupInfo = {
+            id: 2,
+            name: 'Engineering',
+            ownerUserId: 1,
+            adminUserIds: [1],
+            memberUserIds: [1],
+            channelIds: [],
+            joinRequests: [],
+        };
+    }
 }
--- client/src/app/models/channel.model.ts
+++ client/src/app/models/channel.model.ts
@@ -1,8 +1,8 @@
 export interface Channel {
-  id: number;
-  name: string;
-  groupId: number;
-  members: number[];
+    id: number;
+    name: string;
+    groupId: number;
+    members: number[];
 }
 
 /*
